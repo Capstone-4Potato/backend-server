cb63c78b08f297104e47d5d5668f9e2e0a390df0
gabean13 <gazette9@naver.com>
2024-04-02 18:06:22 +0900

    create project

---
+++ src/main/java/com/potato/balbambalbam/BalbambalbamApplication.java
@@ -0,0 +1,13 @@
+package com.potato.balbambalbam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalbambalbamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalbambalbamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/potato/balbambalbam/BalbambalbamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.potato.balbambalbam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalbambalbamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
