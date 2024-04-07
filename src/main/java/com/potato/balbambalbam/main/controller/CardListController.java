5667469ca097d944c398540d60dfc2f35b53cdee
gabean13 <gazette9@naver.com>
2024-04-08 00:30:28 +0900

    [Chore] : Swagger 추가 (#2)

--- src/main/java/com/potato/balbambalbam/main/controller/CardListController.java
+++ src/main/java/com/potato/balbambalbam/main/controller/CardListController.java
@@ -1,9 +1,13 @@
 package com.potato.balbambalbam.main.controller;
 
-import com.potato.balbambalbam.main.dto.Response;
+import com.potato.balbambalbam.main.dto.CardListResponse;
 import com.potato.balbambalbam.main.dto.ResponseCardDto;
 import com.potato.balbambalbam.main.service.CardListService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,15 +17,18 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "CardList", description = "CardList API")
 public class CardListController {
 
     private final CardListService cardListService;
 
     @GetMapping ("/cards")
-    public ResponseEntity<List<ResponseCardDto>> getCardList(@RequestParam("category") String category, @RequestParam("subcategory") String subcategory){
+    @Operation(summary = "Get CardList", description = "해당 카테고리의 카드 리스트를 조회한다.")
+    public ResponseEntity<CardListResponse<List<ResponseCardDto>>> getCardList(@RequestParam("category") String category, @RequestParam("subcategory") String subcategory){
         List<ResponseCardDto> cardDtoList = cardListService.resolveCardListRequest(category, subcategory);
-        return ResponseEntity.ok().body(cardDtoList);
-    }
+        CardListResponse<List<ResponseCardDto>> response = new CardListResponse<>(cardDtoList, cardDtoList.size());
 
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+    }
 
 }
