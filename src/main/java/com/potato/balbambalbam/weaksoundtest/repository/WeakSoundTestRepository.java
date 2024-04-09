167436970c43d3777a187a74fc2e24c37926e586
yoownny <kelosy00@naver.com>
2024-04-10 03:50:33 +0900

    [Feat] : AI와의 요청과 응답 (#5)

---
+++ src/main/java/com/potato/balbambalbam/weaksoundtest/repository/WeakSoundTestRepository.java
@@ -0,0 +1,7 @@
+package com.potato.balbambalbam.weaksoundtest.repository;
+
+import com.potato.balbambalbam.entity.WeakSoundTest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeakSoundTestRepository extends JpaRepository<WeakSoundTest,Long> {
+}
