a9043dc346cf9d5fbdb2f32479678a0d87763cd1
gabean13 <gazette9@naver.com>
2024-04-11 16:50:07 +0900

    [Feat] : card info 에러 처리 추가 (#7)

--- src/main/java/com/potato/balbambalbam/main/MainExceptionResolverController.java
+++ src/main/java/com/potato/balbambalbam/main/MainExceptionResolverController.java
@@ -1,5 +1,6 @@
-package com.potato.balbambalbam.main.cardList.dto;
+package com.potato.balbambalbam.main;
 
+import com.potato.balbambalbam.main.cardList.dto.ExceptionDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class MainExceptionResolverController {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionDto> illegalArgumentExceptionHandler(IllegalArgumentException ex){
+        ExceptionDto exceptionDto = new ExceptionDto("IllegalArgumentException", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ExceptionDto> runtimeExceptionHandler(RuntimeException ex){
         ExceptionDto exceptionDto = new ExceptionDto("IllegalArgumentException", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionDto);
     }
--- src/main/java/com/potato/balbambalbam/main/cardInfo/exception/UserNotFoundException.java
+++ src/main/java/com/potato/balbambalbam/main/cardInfo/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.potato.balbambalbam.main.cardInfo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User Not Found")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "잘못된 요청입니다.")
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message) {
         super(message);
