0391c998b7b6574eddaf147807cf768f757b4511
gabean13 <gazette9@naver.com>
2024-04-13 15:40:33 +0900

    [Refactor] : MainExceptionResolverController 중복코드 제거

--- src/main/java/com/potato/balbambalbam/main/cardInfo/exception/AiConnectionException.java
+++ src/main/java/com/potato/balbambalbam/main/cardInfo/exception/AiConnectionException.java
@@ -1,9 +1,5 @@
 package com.potato.balbambalbam.main.cardInfo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "통신 가능 최대 시간 초과.")
 public class AiConnectionException extends RuntimeException {
     public AiConnectionException(String message) {
         super(message);
--- src/main/java/com/potato/balbambalbam/main/cardInfo/exception/UserNotFoundException.java
+++ src/main/java/com/potato/balbambalbam/main/cardInfo/exception/UserNotFoundException.java
@@ -1,9 +1,5 @@
 package com.potato.balbambalbam.main.cardInfo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "잘못된 요청입니다.")
 public class UserNotFoundException extends RuntimeException{
     public UserNotFoundException(String message) {
         super(message);
