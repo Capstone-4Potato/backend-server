2f6c41c84c8c5ac53e341c4602d056d1313f531f
gabean13 <gazette9@naver.com>
2024-04-13 15:40:33 +0900

    [Refactor] : MainExceptionResolverController 중복코드 제거

--- src/main/java/com/potato/balbambalbam/main/cardList/exception/CardNotFoundException.java
+++ src/main/java/com/potato/balbambalbam/main/cardList/exception/CardNotFoundException.java
@@ -1,9 +1,5 @@
 package com.potato.balbambalbam.main.cardList.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "잘못된 요청입니다.")
 public class CardNotFoundException extends IllegalArgumentException{
     public CardNotFoundException(String s) {
         super(s);
