b8419a595d548eb9c32c8dfd146f22bdf703da3c
Gabin <gazette9@naver.com>
2024-04-16 13:38:10 +0900

    Revert "[Feature] card Feedback"

--- src/main/java/com/potato/balbambalbam/config/WebConfig.java
+++
@@ -1,8 +0,0 @@
-package com.potato.balbambalbam.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class WebConfig {
-
-}
--- src/main/java/com/potato/balbambalbam/main/cardInfo/exception/AiConnectionException.java
+++
@@ -1,7 +0,0 @@
-package com.potato.balbambalbam.main.cardInfo.exception;
-
-public class AiConnectionException extends RuntimeException {
-    public AiConnectionException(String message) {
-        super(message);
-    }
-}
