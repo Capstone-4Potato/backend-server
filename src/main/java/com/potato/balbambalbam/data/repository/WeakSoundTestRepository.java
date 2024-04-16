e0515c157920d3186db6bcb88fba80194583e69f
yoownny <kelosy00@naver.com>
2024-04-16 13:19:02 +0900

    directory structure 수정 (#5)

--- src/main/java/com/potato/balbambalbam/data/repository/WeakSoundTestRepository.java
+++ src/main/java/com/potato/balbambalbam/data/repository/WeakSoundTestRepository.java
@@ -1,6 +1,6 @@
-package com.potato.balbambalbam.repository;
+package com.potato.balbambalbam.data.repository;
 
-import com.potato.balbambalbam.entity.WeakSoundTest;
+import com.potato.balbambalbam.data.entity.WeakSoundTest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WeakSoundTestRepository extends JpaRepository<WeakSoundTest,Long> {
