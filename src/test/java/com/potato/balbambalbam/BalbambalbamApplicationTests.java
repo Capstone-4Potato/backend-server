297b4813e0e553fc2436009325e62fd3936bbfb9
gabean13 <gazette9@naver.com>
2024-04-17 18:53:36 +0900

    [Test] : @SpringBootTest 삭제

--- src/test/java/com/potato/balbambalbam/BalbambalbamApplicationTests.java
+++ src/test/java/com/potato/balbambalbam/BalbambalbamApplicationTests.java
@@ -3,7 +3,6 @@ package com.potato.balbambalbam;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BalbambalbamApplicationTests {
 
 	@Test
