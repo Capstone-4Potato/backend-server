fd85262c9fcd80f79f88484a2547f9bd135078b6
yoownny <kelosy00@naver.com>
2024-04-26 18:32:20 +0900

    [Refactor] : User entity에 voice 삭제 (#21)

--- src/main/java/com/potato/balbambalbam/data/entity/User.java
+++ src/main/java/com/potato/balbambalbam/data/entity/User.java
@@ -21,8 +21,6 @@ public class User {
     private Byte gender; // 0 : 여성, 1 : 남성
     @Column(name = "email", nullable = false, unique = true)
     private String email;
-    @Column(name = "voice", nullable = false)
-    private Integer voice;
 
     public User() {
     }
