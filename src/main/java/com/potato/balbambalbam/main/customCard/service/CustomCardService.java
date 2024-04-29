1eb035912d86428bbe7ab302ba9b1a06419cbd0a
gabean13 <gazette9@naver.com>
2024-04-29 17:06:09 +0900

    [Feat] : custom card 생성 기준 추가 (#22)

--- src/main/java/com/potato/balbambalbam/main/customCard/service/CustomCardService.java
+++ src/main/java/com/potato/balbambalbam/main/customCard/service/CustomCardService.java
@@ -4,11 +4,13 @@ import com.potato.balbambalbam.data.entity.CustomCard;
 import com.potato.balbambalbam.data.entity.User;
 import com.potato.balbambalbam.data.repository.CustomCardRepository;
 import com.potato.balbambalbam.data.repository.UserRepository;
-import com.potato.balbambalbam.main.exception.CardCapacityExceededException;
+import com.potato.balbambalbam.main.exception.CardGenerationFailException;
 import com.potato.balbambalbam.main.exception.CardNotFoundException;
 import com.potato.balbambalbam.main.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
 
 @Service
 @RequiredArgsConstructor
@@ -18,9 +20,9 @@ public class CustomCardService {
     private final UserRepository userRepository;
     private final AiPronunciationService aiPronunciationService;
 
-    public Long createCustomCardIfPossible (String text, Long userId) throws CardCapacityExceededException {
-        if(!canGenerateSentence(userId)){
-            throw new CardCapacityExceededException("카드를 더 이상 생성할 수 없습니다");
+    public Long createCustomCardIfPossible (String text, Long userId) throws CardGenerationFailException {
+        if(!canGenerateSentence(userId, text)){
+            throw new CardGenerationFailException("카드를 생성할 수 없습니다");
         }
 
         return createCustomCard(text, userId).getId();
@@ -38,10 +40,20 @@ public class CustomCardService {
         return true;
     }
 
-    protected boolean canGenerateSentence(Long userId){
+    protected boolean canGenerateSentence(Long userId, String text){
         User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("존재하지 않는 회원입니다"));
 
+        //기존 커스텀 문장 10개 이상이면 생성 불가
         if(customCardRepository.findAllByUserId(userId).size() >= 10){
+            return false;
+        }
+        //문장에 영어가 있으면 생성 불가
+        if(!Pattern.matches("^[ㄱ-ㅎ가-힣]*$", text)){
+            return false;
+        }
+
+        //문장이 35자 이상이면 생성 불가
+        if(text.length() >= 35){
             return false;
         }
 
