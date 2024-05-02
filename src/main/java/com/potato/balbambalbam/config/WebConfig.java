44ae47a5f48e136148ebe3a9cc250b1d0816fbc2
gabean13 <gazette9@naver.com>
2024-05-02 14:15:20 +0900

    [Feat] : LogInterceptor 추가 (#22)

--- src/main/java/com/potato/balbambalbam/config/WebConfig.java
+++ src/main/java/com/potato/balbambalbam/config/WebConfig.java
@@ -1,9 +1,17 @@
 package com.potato.balbambalbam.config;
 
+import com.potato.balbambalbam.main.interceptor.LogInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
+    }
 }
