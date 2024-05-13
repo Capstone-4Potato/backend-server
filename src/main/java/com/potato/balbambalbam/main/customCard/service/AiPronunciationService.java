226d2cefa99b1ca06da80d21873f2a6074d4823b
gabean13 <gazette9@naver.com>
2024-05-13 14:55:14 +0900

    [Feat] : 인공지능 통신 시 클라이언트까지 인공지능 에러 메세지 전달 추가 (#22)

--- src/main/java/com/potato/balbambalbam/main/customCard/service/AiPronunciationService.java
+++ src/main/java/com/potato/balbambalbam/main/customCard/service/AiPronunciationService.java
@@ -1,10 +1,13 @@
 package com.potato.balbambalbam.main.customCard.service;
 
+import com.potato.balbambalbam.exception.AiGenerationFailException;
+import com.potato.balbambalbam.exception.InvalidParameterException;
 import com.potato.balbambalbam.main.customCard.dto.AiPronunciationRequestDto;
 import com.potato.balbambalbam.main.customCard.dto.AiPronunciationResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,6 +30,12 @@ public class AiPronunciationService {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(Mono.just(aiPronunciationRequestDto), AiPronunciationRequestDto.class)
                 .retrieve()//요청
+                //에러 처리 : 요청이 잘못갔을 경우
+                .onStatus(HttpStatus.BAD_REQUEST::equals,
+                        response-> response.bodyToMono(String.class).map(InvalidParameterException::new))
+                //에러 처리 : 발음생성실패
+                .onStatus(HttpStatus.INTERNAL_SERVER_ERROR::equals,
+                        response-> response.bodyToMono(String.class).map(AiGenerationFailException::new))
                 .bodyToMono(AiPronunciationResponseDto.class)
                 .timeout(Duration.ofSeconds(2)) //2초 안에 응답 오지 않으면 TimeoutException 발생
                 .block();
