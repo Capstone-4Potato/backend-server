c2d4916bf42ec28758ddc092bb45b495ea1ea545
gabean13 <gazette9@naver.com>
2024-05-13 17:12:49 +0900

    [Fix] : error runtime or Illegalexception으로 통일

--- src/main/java/com/potato/balbambalbam/exception/CardDeleteException.java
+++ src/main/java/com/potato/balbambalbam/exception/CardDeleteException.java
@@ -1,6 +1,6 @@
 package com.potato.balbambalbam.exception;
 
-public class CardDeleteException extends Exception {
+public class CardDeleteException extends RuntimeException{
     public CardDeleteException(String message) {
         super(message);
     }
