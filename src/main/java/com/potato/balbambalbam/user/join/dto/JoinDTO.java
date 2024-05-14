d5e0c895a14beab0151a6ed927a8688a476a5f02
gabean13 <gazette9@naver.com>
2024-05-14 16:08:54 +0900

    [Fix] : joindto 수정

--- src/main/java/com/potato/balbambalbam/user/join/dto/JoinDTO.java
+++ src/main/java/com/potato/balbambalbam/user/join/dto/JoinDTO.java
@@ -1,15 +1,19 @@
 package com.potato.balbambalbam.user.join.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
 public class JoinDTO {
-
+    @NotBlank
     private String name;
+    @NotBlank
     private String socialId;
+    @NotBlank
     private Integer age;
+    @NotBlank
     private Byte gender;
 
 }
