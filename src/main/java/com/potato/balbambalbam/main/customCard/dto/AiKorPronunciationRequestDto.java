275bdbfeb59a384a490feed8bc80fa071a8c1328
gabean13 <gazette9@naver.com>
2024-05-14 13:31:31 +0900

    [Feat] : customCard 로마자 발음 추가 (#22)

---
+++ src/main/java/com/potato/balbambalbam/main/customCard/dto/AiKorPronunciationRequestDto.java
@@ -0,0 +1,9 @@
+package com.potato.balbambalbam.main.customCard.dto;
+
+public class AiKorPronunciationRequestDto {
+
+    public String text;
+    public AiKorPronunciationRequestDto(String text) {
+        this.text = text;
+    }
+}
