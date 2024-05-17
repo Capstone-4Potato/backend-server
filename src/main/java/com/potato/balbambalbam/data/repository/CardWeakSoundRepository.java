ac68b8453ca14f3bb66b46af87c458f8573abf3c
gabean13 <gazette9@naver.com>
2024-05-17 15:27:39 +0900

    [Feat] : 취약음 갱신 시 카드 취약음 여부 갱신 (#2)

--- src/main/java/com/potato/balbambalbam/data/repository/CardWeakSoundRepository.java
+++ src/main/java/com/potato/balbambalbam/data/repository/CardWeakSoundRepository.java
@@ -2,6 +2,7 @@ package com.potato.balbambalbam.data.repository;
 
 import com.potato.balbambalbam.data.entity.CardWeakSound;
 import com.potato.balbambalbam.data.entity.CardWeakSoundId;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,4 +13,6 @@ public interface CardWeakSoundRepository extends JpaRepository<CardWeakSound, Ca
 
     boolean existsByCardIdAndUserId(Long cardId, Long userId);
     Optional<CardWeakSound> findByCardId(Long cardId);
+    @Transactional
+    void deleteAllByUserId(Long userId);
 }
