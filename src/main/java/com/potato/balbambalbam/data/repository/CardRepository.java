1b138b134483c6f5666379aa1874f7d3184c81fe
gabean13 <gazette9@naver.com>
2024-08-25 23:57:09 +0900

    [Feat] : card Voice entity update, card voice repository update

--- src/main/java/com/potato/balbambalbam/data/repository/CardRepository.java
+++ src/main/java/com/potato/balbambalbam/data/repository/CardRepository.java
@@ -2,6 +2,7 @@ package com.potato.balbambalbam.data.repository;
 
 import com.potato.balbambalbam.data.entity.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,7 +12,8 @@ public interface CardRepository extends JpaRepository<Card, Long> {
     List<Card> findAll();
     List<Card> findAllByCategoryId(Long id);
     List<Card> findByCategoryIdIn(List<Long> categoryIds);
-
     Optional<Card> findById(Long id);
     List<Card> findByTextOrderById(String text);
+    @Query(value = "select * from card c where c.id not in (select cv.id from card_voice cv)", nativeQuery = true)
+    List<Card> findCardNotInCardVoice();
 }
