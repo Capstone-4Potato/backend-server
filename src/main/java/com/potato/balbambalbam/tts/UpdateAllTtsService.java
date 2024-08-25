c931ace49d629ff6d6d1403ead52edf5c0ebac07
gabean13 <gazette9@naver.com>
2024-08-26 00:03:05 +0900

    [Feat] : 음성이 존재하지 않으면 음성 저장, 스프링 실행 시 작동하도록 추가 (#68)

---
+++ src/main/java/com/potato/balbambalbam/tts/UpdateAllTtsService.java
@@ -0,0 +1,48 @@
+package com.potato.balbambalbam.tts;
+
+import com.potato.balbambalbam.data.entity.Card;
+import com.potato.balbambalbam.data.entity.CardVoice;
+import com.potato.balbambalbam.data.repository.CardRepository;
+import com.potato.balbambalbam.data.repository.CardVoiceRepository;
+import com.potato.balbambalbam.tts.dto.AiAllTtsResponseDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UpdateAllTtsService {
+    private final AiAllTtsService aiAllTtsService;
+    private final CardRepository cardRepository;
+    private final CardVoiceRepository cardVoiceRepository;
+
+    public void saveAllTtsToFile() {
+        List<Card> emptyTtsCardList = cardRepository.findCardNotInCardVoice();
+        emptyTtsCardList.forEach(card -> getAiVoice(card));
+    }
+
+    protected void getAiVoice(Card card) {
+        saveSixVoices(card.getId(), aiAllTtsService.getAiTtsResponse(card.getText()));
+    }
+
+    @Transactional
+    protected void saveSixVoices(Long id, AiAllTtsResponseDto allTtsResponseDto) {
+
+        CardVoice cardvoice = CardVoice.builder()
+                .id(id)
+                .childMale(allTtsResponseDto.getChild_0())
+                .childFemale(allTtsResponseDto.getChild_1())
+                .adultMale(allTtsResponseDto.getAdult_0())
+                .adultFemale(allTtsResponseDto.getAdult_1())
+                .elderlyMale(allTtsResponseDto.getElderly_0())
+                .elderlyFemale(allTtsResponseDto.getElderly_1())
+                .build();
+
+        cardVoiceRepository.save(cardvoice);
+        log.info("{}번 음성 저장 완료", id);
+    }
+}
