5f29b6be0048173beea33b8dbfb4a4ee1e92f61b
yoownny <kelosy00@naver.com>
2024-09-25 03:57:09 +0900

    [Feat] : log 데이터베이스에 입력 (#77)

--- src/main/java/com/potato/balbambalbam/exception/controller/MainExceptionResolverController.java
+++ src/main/java/com/potato/balbambalbam/exception/controller/MainExceptionResolverController.java
@@ -1,7 +1,9 @@
 package com.potato.balbambalbam.exception.controller;
 
 import com.potato.balbambalbam.exception.*;
+import com.potato.balbambalbam.log.ExceptionLogService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -18,19 +20,27 @@ import java.util.concurrent.TimeoutException;
 @Slf4j
 @RestControllerAdvice
 public class MainExceptionResolverController extends ResponseEntityExceptionHandler {
+
+    private final ExceptionLogService exceptionLogService;
+
+    @Autowired
+    public MainExceptionResolverController(ExceptionLogService exceptionLogService) {
+        this.exceptionLogService = exceptionLogService;
+    }
+
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        exceptionLogService.logException(ex, HttpStatus.valueOf(statusCode.value()), request);
         String className = extractClassName(ex.getClass().toString());
-
         ExceptionDto exceptionDto = new ExceptionDto(statusCode.value(), className, ex.getMessage());
         return new ResponseEntity<>(exceptionDto, headers, statusCode);
     }
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        exceptionLogService.logException(ex, HttpStatus.valueOf(status.value()), request);
         String className = extractClassName(ex.getClass().toString());
         String defaultMessage = ex.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
-
         ExceptionDto exceptionDto = new ExceptionDto(status.value(), className, defaultMessage);
         return new ResponseEntity<>(exceptionDto, headers, status);
     }
@@ -38,45 +48,45 @@ public class MainExceptionResolverController extends ResponseEntityExceptionHand
     @ExceptionHandler({IllegalArgumentException.class, UserNotFoundException.class,
             CategoryNotFoundException.class, CardNotFoundException.class,
             InvalidParameterException.class, ResponseNotFoundException.class})
-    public ResponseEntity<ExceptionDto> notFoundExceptionHandler(Exception ex){
-        return exceptionHandler(ex, HttpStatus.NOT_FOUND); //404
+    public ResponseEntity<ExceptionDto> notFoundExceptionHandler(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        exceptionLogService.logException(ex, status, request);
+        return exceptionHandler(ex, status);
     }
 
     @ExceptionHandler({RuntimeException.class, AiConnectionException.class,
             TimeoutException.class, CardDeleteException.class,
             WebClientResponseException.InternalServerError.class,
             AiGenerationFailException.class, VoiceNotFoundException.class})
-    public ResponseEntity<ExceptionDto> timeoutExceptionHandler(Exception ex){
-        return exceptionHandler(ex, HttpStatus.INTERNAL_SERVER_ERROR); //500
+    public ResponseEntity<ExceptionDto> internalServerErrorExceptionHandler(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        exceptionLogService.logException(ex, status, request);
+        return exceptionHandler(ex, status);
     }
 
     @ExceptionHandler({SocialIdChangeException.class, InvalidUserNameException.class,
             ParameterNotFoundException.class})
-    public ResponseEntity<ExceptionDto> badRequestExceptionHandler(Exception ex) {
-        return exceptionHandler(ex, HttpStatus.BAD_REQUEST); //400
+    public ResponseEntity<ExceptionDto> badRequestExceptionHandler(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        exceptionLogService.logException(ex, status, request);
+        return exceptionHandler(ex, status);
     }
 
     @ExceptionHandler({TokenExpiredException.class})
-    public ResponseEntity<ExceptionDto> unauthorizedExceptionHandler(Exception ex) {
-        return exceptionHandler(ex, HttpStatus.UNAUTHORIZED); //401
+    public ResponseEntity<ExceptionDto> unauthorizedExceptionHandler(Exception ex, WebRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        exceptionLogService.logException(ex, status, request);
+        return exceptionHandler(ex, status);
     }
 
-    /**
-     * 에러 메서드 처리
-     * @param ex
-     * @param httpStatus
-     * @return
-     */
-    protected ResponseEntity<ExceptionDto> exceptionHandler(Exception ex, HttpStatusCode httpStatus){
+    protected ResponseEntity<ExceptionDto> exceptionHandler(Exception ex, HttpStatusCode httpStatus) {
         String className = extractClassName(ex.getClass().toString());
         String exMessage = ex.getMessage();
-
-        log.error("[ERROR] [{}]: {}" ,className, exMessage);
-
+        log.error("[ERROR] [{}]: {}", className, exMessage);
         return ResponseEntity.status(httpStatus).body(new ExceptionDto(httpStatus.value(), className, exMessage));
     }
 
-    protected String extractClassName(String fullClassName){
+    protected String extractClassName(String fullClassName) {
         String[] classNameParts = fullClassName.split("\\.");
         return classNameParts[classNameParts.length - 1];
     }
