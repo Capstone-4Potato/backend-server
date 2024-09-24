cb399aa657f1bbd6b7d6b0f9d649566f6a19b4af
yoownny <kelosy00@naver.com>
2024-09-25 04:06:21 +0900

    [Feat] : exception_info 중복 제거 (#77)

--- src/main/java/com/potato/balbambalbam/log/repository/ExceptionInfoRepository.java
+++ src/main/java/com/potato/balbambalbam/log/repository/ExceptionInfoRepository.java
@@ -3,5 +3,9 @@ package com.potato.balbambalbam.log.repository;
 import com.potato.balbambalbam.log.entity.ExceptionInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ExceptionInfoRepository extends JpaRepository<ExceptionInfo, Long> {
+    Optional<ExceptionInfo> findByExceptionNameAndExceptionMessageAndExceptionHttpStatus(
+            String exceptionName, String exceptionMessage, int exceptionHttpStatus);
 }
