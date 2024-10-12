10f2bee04f2c86053dff8655b45e1fd5cffd114a
gabean13 <choegabin171@gmail.com>
2024-10-12 17:07:13 +0900

    [Rename] : file directory 위치 변경

---
+++ src/main/java/com/potato/balbambalbam/home/learningCourse/dto/EngTranslationRequestDto.java
@@ -0,0 +1,12 @@
+package com.potato.balbambalbam.card.cardList.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class EngTranslationRequestDto {
+    private String text;
+}
